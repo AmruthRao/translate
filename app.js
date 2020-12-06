368ee11ae0fb7d2de9840dc90628ff496c385198
AmruthRao <54882912+AmruthRao@users.noreply.github.com>
2020-12-06 13:37:25 +0530

    call back function
    Tried querySelector with a tag,class,id and an attribute.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var btnTranslate=document.querySelector("#btn-Translate")
+var txtInput1=document.querySelector("textarea")
+var txtInput2=document.querySelector("#txt-input")
+var txtInput3=document.querySelector(".input")
+var txtInput4=document.querySelector("[name='textinput']")
 function btnclickHandler(){
     console.log("clicked")
+    console.log(txtInput1.value)
+    console.log(txtInput2value)
+    console.log(txtInput3.value)
+    console.log(txtInput4.value)
  }
 btnTranslate.addEventListener("click",btnclickHandler)
 
